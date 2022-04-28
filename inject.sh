863f5af81e77f17b30fbe1f74311cd542b2078b4
Hongru  Liu <hrliu@alauda.io>
2022-04-28 11:55:25 +0800

    change inject.sh

--- inject.sh
+++ inject.sh
@@ -1,3 +1,3 @@
-docker rm -f redis-cluster-docker-compose_redis-node1 redis-cluster-docker-compose_redis-node2 redis-cluster-docker-compose_redis-node3
+docker stop  redis-cluster-docker-compose_redis-node1 redis-cluster-docker-compose_redis-node2 redis-cluster-docker-compose_redis-node3
 redis-cli -p 7003 cluster nodes 
 redis-cli -p 7003 -c get 1
